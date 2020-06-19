9ff857688994e5b5f977d699c775efbe735e8b11
Vinicius Pascucci <861863@cognizant.com>
2020-06-18 23:48:18 -0300

    First Commit Dojo 2

---
+++ src/main/java/br/com/cognizant/cog/Test.java
@@ -0,0 +1,11 @@
+package br.com.cognizant.cog;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Teste !!");
+
+	}
+
+}
---
+++ src/test/java/br/com/cognizant/teste/MainTest.java
@@ -0,0 +1,13 @@
+package br.com.cognizant.teste;
+import org.junit.Test;
+
+
+public class MainTest {
+
+	@Test
+	public void testMain() {
+		
+		// TODO: Test
+	}
+	
+}
